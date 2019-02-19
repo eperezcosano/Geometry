c872f4cfc9cc77aecbdd58db139c5a3858f2e021
Izan Pérez Cosano <izanperez98@gmail.com>
2019-02-19 12:58:56 +0100

    main included

--- src/GestorFigures.java
+++ src/GestorFigures.java
@@ -1,5 +1,5 @@
 public interface GestorFigures {
-    public static double suma (Figura [] l) {
+    public static double suma(Figura [] l) {
         double s = 0;
         for (Figura f: l) {
             s += f.area();
