69de14df0e46369d1b5af23d4b784f28e5e199a4
Izan Pérez Cosano <izanperez98@gmail.com>
2019-02-19 13:19:00 +0100

    changed interface for class

--- src/GestorFigures.java
+++ src/GestorFigures.java
@@ -1,13 +1,13 @@
 import java.util.Arrays;
-public interface GestorFigures {
-    static double suma(Figura [] l) {
+public class GestorFigures {
+    public static double suma(Figura[] l) {
         double s = 0;
         for (Figura f: l) {
             s += f.area();
         }
         return s;
     }
-    static void sort(Figura[] l) {
+    public static void sort(Figura[] l) {
         Arrays.sort(l);
     }
 }
