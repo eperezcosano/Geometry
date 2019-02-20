f652990250259f1e9cf518368137824ce4d54e7c
Izan Pérez Cosano <izanperez98@gmail.com>
2019-02-20 21:54:12 +0100

    commented and correction this.l1 this.l2

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,14 +1,18 @@
 public class Rectangle extends Figura {
+
+    //Atributes
     public double l1;
     public double l2;
 
+    //Constructor
     public Rectangle(double l1, double l2) {
         this.l1 = l1;
         this.l2 = l2;
     }
 
+    //Method area from the abstract class Figura
     @Override
     public double area() {
-        return l1 * l2;
+        return this.l1 * this.l2;
     }
 }
