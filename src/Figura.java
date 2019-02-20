0f62419f921b21e31bf70ab8771c1fda3862f8ab
Izan Pérez Cosano <izanperez98@gmail.com>
2019-02-20 21:51:52 +0100

    commented

--- src/Figura.java
+++ src/Figura.java
@@ -1,6 +1,9 @@
-public abstract class Figura implements Comparable<Figura>{
+public abstract class Figura implements Comparable<Figura> {
+
+    //Abtract method area
     public abstract double area();
 
+    //Requeriment for sorting condition
     @Override
     public int compareTo(Figura o) {
         return (int)(this.area() - o.area());
