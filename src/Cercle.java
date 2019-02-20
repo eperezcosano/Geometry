48f1980fe6e06e5d3c1e13db5360bd547d56b110
Izan Pérez Cosano <izanperez98@gmail.com>
2019-02-20 21:47:46 +0100

    commented and correction this.r

--- src/Cercle.java
+++ src/Cercle.java
@@ -1,12 +1,16 @@
 public class Cercle extends Figura {
+
+    //Atributes
     public double r;
 
+    //Constructor
     public Cercle(double r) {
         this.r = r;
     }
 
+    //Method area from the abstract class Figura
     @Override
     public double area() {
-        return Math.PI * Math.pow(r, 2);
+        return Math.PI * Math.pow(this.r, 2);
     }
 }
