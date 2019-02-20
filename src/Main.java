4b3eb32435ef76b1887d6c772bfbf7241fda93a6
Izan Pérez Cosano <izanperez98@gmail.com>
2019-02-20 21:45:09 +0100

    commented

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+
+        //Print my first "Hello World!"
         System.out.println("Hello World!");
 
-        Triangle t = new Triangle(2,3);
+        //Create some shapes for testing
+        Triangle t = new Triangle(2, 3);
         Rectangle r = new Rectangle(3, 4);
         Cercle c = new Cercle(3);
         Quadrat q = new Quadrat(2);
 
+        //Put all of them in a array
         Figura[] f = new Figura[5];
         f[0] = t;
         f[1] = r;
@@ -14,9 +18,11 @@ public class Main {
         f[3] = c;
         f[4] = q;
 
+        //Sum all the areas of the array
         double suma = GestorFigures.suma(f);
         System.out.println(suma);
 
+        //Sort the array by the shape's area
         GestorFigures.sort(f);
         for (Figura l: f) {
             System.out.println(l.getClass().getName() + " " + l.area());
