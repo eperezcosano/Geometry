ed30993b3d51e9f9611a40e7ce534341be931816
Izan Pérez Cosano <izanperez98@gmail.com>
2019-02-20 21:55:03 +0100

    commented and correction this.b this.h

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,14 +1,18 @@
 public class Triangle extends Figura {
+
+    //Atributes
     public double b;
     public double h;
 
+    //Constructor
     public Triangle(double b, double h) {
         this.b = b;
         this.h = h;
     }
 
+    //Method area from the abstract class Figura
     @Override
     public double area() {
-        return (b * h) / 2;
+        return (this.b * this.h) / 2;
     }
 }
