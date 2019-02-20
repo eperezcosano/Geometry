24b3c410426294cbb8ce41976237248d9a3c6799
Izan Pérez Cosano <izanperez98@gmail.com>
2019-02-20 21:50:02 +0100

    commented

--- src/GestorFigures.java
+++ src/GestorFigures.java
@@ -1,5 +1,8 @@
 import java.util.Arrays;
+
 public interface GestorFigures {
+
+    //Return the sum of all shapes areas given an array of Figura
     static double suma(Figura [] l) {
         double s = 0;
         for (Figura f: l) {
@@ -7,6 +10,8 @@ public interface GestorFigures {
         }
         return s;
     }
+
+    //Sort an array of Figura by its areas
     static void sort(Figura[] l) {
         Arrays.sort(l);
     }
